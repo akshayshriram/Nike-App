f08d0c4e54c2de439234c37f3e48a37ca5719ee2
Akshay <akshayshriram999@gmail.com>
2025-01-12 12:18:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <main className="relative">
+        <section className="xl:padding-1 wide:padding-r padding-b"></section>
+        <section className="padding">PopularProducts</section>
+        <section className="padding">SuperQuality</section>
+        <section className="padding-x py-10">Services</section>
+        <section className="padding">Special services</section>
+        <section className="padding">CustomerReview</section>
+        <section className="padding">Subscribe</section>
+        <section className="padding">Footer</section>
+      </main>
+    </>
+  );
+};
+
+export default App;
