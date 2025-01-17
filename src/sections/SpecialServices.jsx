50565545e6e0cf1922cfbfa69ab0308a011d004c
Akshay <akshayshriram999@gmail.com>
2025-01-17 13:09:18 +0530

    Add Special Services...

--- src/sections/SpecialServices.jsx
+++ src/sections/SpecialServices.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
 
 const SpecialServices = () => {
-  return <div>SpecialServices</div>;
+  return (
+    <>
+      <section className="max-container flex justify-center flex-wrap gap-9">
+        {services.map((service) => (
+          <ServiceCard key={service} {...service} />
+        ))}
+      </section>
+    </>
+  );
 };
 
 export default SpecialServices;
